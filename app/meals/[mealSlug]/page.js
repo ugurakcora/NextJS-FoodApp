51e7b885d730c42ce8ad16931e10a71594b378b2
ugurakcora <uakcora97@gmail.com>
2024-10-06 05:30:59 +0300

    feat: init

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealsDetailPage() {
+  return (
+    <div>
+      <h1>Meals Detail Page</h1>
+    </div>
+  );
+}
---
+++ app/meals/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <div>
+      <h1>Meals Page</h1>
+    </div>
+  );
+}
